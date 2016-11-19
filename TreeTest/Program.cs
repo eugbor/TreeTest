fd92d3375d5d179e1170eb8a1ca6d4cd37ff0806
eugbor <eugeneborodin@mail.ru>
2016-11-20 01:49:56 +0300

    commit

--- TreeTest/Program.cs
+++ TreeTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TreeTest
 {
@@ -8,8 +9,23 @@ namespace TreeTest
         {
             int n = int.Parse(Console.ReadLine());
             Tree tree = new Tree();
-            
-           
+            var rnd = new Random();
+            for (var i = 0; i < n; i++)
+                tree.Add(i, rnd.Next(100));
+
+            // Вывод дерева на консоль
+            foreach (var pair in tree.GetView())
+                Console.WriteLine("{0} : {1}", pair.Key, pair.Value);
+
+
+            var value = int.Parse(Console.ReadLine());
+
+            // Поиск значания (Левосторонний обход)
+            var path = tree.Find(value);
+            if (path.Any())
+                Console.WriteLine(path);
+            else
+                Console.WriteLine("Не найдено");
 
             Console.ReadKey();
         }
--- TreeTest/Tree.cs
+++ TreeTest/Tree.cs
@@ -5,24 +5,37 @@ namespace TreeTest
 {
     public class Tree
     {
+        /// <summary>
+        /// Корень
+        /// </summary>
         public TreeItem Root { get; set; }
 
-        public Tree() // это конструктор
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public Tree()
         {
-            Root = new TreeItem(); // распределить память для объекта типа TreeItem // конструктор TreeItem() вызывается для свойства Root
+            Root = new TreeItem();
         }
 
         /// <summary>
         /// Добавляет указанное значение по указанному индексу
         /// </summary>
-        /// <param name="index"></param>
-        /// <param name="value"></param>
+        /// <param name="index">Индекс</param>
+        /// <param name="value">Значение</param>
         public void Add(int index, int value)
         {
             // рекурсивное добавление
             Add(new List<TreeItem> {Root}, 0, index, value);
         }
 
+        /// <summary>
+        /// Добавляет в список узлов по индексу значение
+        /// </summary>
+        /// <param name="nodeList">Список узлов</param>
+        /// <param name="maxIndex">Максимальный индекс</param>
+        /// <param name="index">Индекс</param>
+        /// <param name="value">Значение</param>
         private void Add(List<TreeItem> nodeList, int maxIndex, int index, int value)
         {
             var nextLevel = new List<TreeItem>();
@@ -50,33 +63,121 @@ namespace TreeTest
             Add(nextLevel, maxIndex, index, value);
         }
 
+        /// <summary>
+        /// Получае представление
+        /// </summary>
+        /// <returns>Коллекция ключей и значений</returns>
         public Dictionary<string, int> GetView()
         {
             Random r = new Random();
 
             Dictionary<string, int> view = new Dictionary<string, int>();
 
-            for (int i = 0; i < 7; i++) // т.к. всего 6 узлов
-            {
-                view.Add(i.ToString(), r.Next(10));
-            }
+            SetView(view, Root, new List<int>());
+
             return view;
-        
-            // представление массива в нормальном виде
            
+        }
+
+        /// <summary>
+        /// Устанавливает представление
+        /// </summary>
+        /// <param name="views">Коллекция ключей и значений</param>
+        /// <param name="node">Узел</param>
+        /// <param name="path">Путь</param>
+        private void SetView(Dictionary<string, int> views, TreeItem node, List<int> path)
+        {
+            path.Add(node.Index);
+
+            // Сцепляет указанные элементы списка строк, помещая между ними заданный разделитель
+            views.Add(string.Join("-", path), node.Value);
+
+            if (node.LeftNode != null)
+                SetView(views, node.LeftNode, path);
+
+            if (node.RightNode != null)
+                SetView(views, node.RightNode, path);
+
+            // Удаляет элемент списка List<int> с указанным индексом
+            path.RemoveAt(path.Count - 1);
+        }
+
+        /// <summary>
+        /// Формирует сроку пути до искомого значения
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <returns>Путь в виде строки</returns>
+        public string Find(int value)
+        {
+            return String.Join("-", Find(value, Root, new List<int>()));
+        }
+
+        /// <summary>
+        /// Ищит значение
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="node">Узел</param>
+        /// <param name="path">Путь</param>
+        /// <returns>Путь</returns>
+        private List<int> Find(int value, TreeItem node, List<int> path)
+        {
+            path.Add(node.Index);
+
+            if (node.Value == value)
+                return path;
+
+            var lengthPath = path.Count;
+
+            if (node.LeftNode != null)
+            {
+                path = Find(value, node.LeftNode, path);
+
+                if (lengthPath != path.Count)
+                    return path;
+            }
+
+            if (node.RightNode != null)
+            {
+                path = Find(value, node.RightNode, path);
+
+                if (lengthPath != path.Count)
+                    return path;
+            }
+            
+
+            path.RemoveAt(path.Count - 1);
+
+            return path;
         } 
     }
 
+
     public class TreeItem
     {
+        /// <summary>
+        /// Индекс
+        /// </summary>
         public int Index { get; set; }
-
+        
+        /// <summary>
+        /// Значение
+        /// </summary>
         public int Value { get; set; }
 
+        /// <summary>
+        /// Левый узел
+        /// </summary>
         public TreeItem LeftNode { get; set; }
 
+        /// <summary>
+        /// Правый узел
+        /// </summary>
         public TreeItem RightNode { get; set; }
 
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="index"></param>
         public TreeItem(int index = 0)
         {
             Index = index;
